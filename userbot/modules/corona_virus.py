8692a122eaa04710ce38f0b22c9e7202505df533
MoveAngel <moveangel29@gmail.com>
2020-03-22 12:00:28 +0700

    Test again

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -12,19 +12,20 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.corona(?: |$)(.*)")
-async def corona(event):
+async def corona(message):
+    args = message.text.split(None, 1)
     covid = Covid()
     data = covid.get_data()
-    input_str = event.pattern_match.group(1)
+    input_str = args[1]
     country = input_str.capitalize()
-    country_data = get_country_data(country.capitalize(), data)
+    country_data = get_country_data(country, data)
     output_text = "`Confirmed : {}\n`".format(country_data["confirmed"])
     output_text += "`Active : {}`\n".format(country_data["active"])
     output_text += "`Deaths : {}`\n".format(country_data["deaths"])
     output_text += "`Recovered : {}`\n".format(country_data["recovered"])
     output_text += "`Last update : {}`\n". \
         format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
-    await event.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+    await message.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
 
 
 def get_country_data(country, world):
