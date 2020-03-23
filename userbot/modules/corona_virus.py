020bfc386101255a1408c8ffc241297912a310e4
MoveAngel <moveangel29@gmail.com>
2020-03-23 23:45:19 +0700

    Test again plox

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -5,29 +5,21 @@
 #
 # Port to UserBot by @MoveAngel
 
-from datetime import datetime
-
 from covid import Covid
 from userbot import CMD_HELP
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.corona(?: |$)(.*)")
-async def corona(message):
+@register(outgoing=True, pattern="^.covid(?: |$)(.*)")
+async def corona(event):
     covid = Covid()
     data = covid.get_data()
     input_str = event.pattern_match.group(1)
     country = input_str.capitalize()
     country_data = get_country_data(country, data)
-    output_text = "`Confirmed : {}\n`".format(country_data["confirmed"])
-    output_text += "`Active : {}`\n".format(country_data["active"])
-    output_text += "`Deaths : {}`\n".format(country_data["deaths"])
-    output_text += "`Recovered : {}`\n".format(country_data["recovered"])
-    output_text += "`Last update : {}`\n". \
-        format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+    output_text = "" 
     for name, value in country_data.items():
         output_text += "`{}`: `{}`\n".format(str(name), str(value))
-    await event.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
-
+    await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
 
 def get_country_data(country, world):
     for country_data in world:
@@ -38,6 +30,6 @@ def get_country_data(country, world):
     
 CMD_HELP.update({
         "covid": 
-        ".corona <country>\
+        ".covid <country>\
           \nUsage: Get an information about data covid-19 in your country.\n"
     })
