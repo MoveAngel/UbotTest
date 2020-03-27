66b190240b92018492553fcba52445a58a905ed2
MoveAngel <moveangel29@gmail.com>
2020-03-28 06:07:00 +0700

    Test

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -10,43 +10,46 @@ from covid import Covid
 from userbot import CMD_HELP
 from userbot.events import register
 
-covid_str = (
-    "**{country}:**  ✅ `{confirmed}` 🦠 `{active}` ⚠️ `{critical}` 💀 `{deaths}` 💚 `{recovered}`"
-)
-
-
 @register(outgoing=True, pattern="^.covid(?: |$)(.*)")
-async def covid19(event):
-    """Get the current covid stats for a specific country or overall."""
-    covid = Covid(source="worldometers")
-    match = event.matches[0].group(1)
-    if match:
-        strings = []
-        args, _ = await client.parse_arguments(match)
-        if match.lower() == "countries":
-            strings = sorted(covid.list_countries())
-        else:
-            for c in args:
-                try:
-                    country = covid.get_status_by_country_name(c)
-                    strings.append(covid_str.format(**country))
-                except ValueError:
-                    continue
-        if strings:
-            await event.answer(',\n'.join(strings))
+async def corona(message):
+    await message.edit("`Processing...`")
+    args = message.text.split(None, 1)
+    if len(args) == 1:
+        url = 'https://covid-19-api-2-i54peomv2.now.sh/api/og'
+        response = requests.get(url, stream=True)
+        with open('og', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        os.rename("og", "og.png")
+        await bot.send_photo(message.chat.id, "og.png", caption="<a href=\"https://covid-19-api-2-i54peomv2.now.sh"
+                                                                   "/api/og\">Source</a>")
+        await message.delete()
+        os.remove("og.png")
+        return
+    covid = Covid()
+    data = covid.get_data()
+    country = args[1]
+    country_data = get_country_data(country.capitalize(), data)
+    if country_data:
+        output_text = "`Confirmed   : {}\n`".format(country_data["confirmed"])
+        output_text += "`Active      : {}`\n".format(country_data["active"])
+        output_text += "`Deaths      : {}`\n".format(country_data["deaths"])
+        output_text += "`Recovered   : {}`\n".format(country_data["recovered"])
+        output_text += "`Last update : {}`\n". \
+            format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+        output_text += "`Data provided by `<a href=\"https://j.mp/2xf6oxF\">Johns Hopkins University</a>"
     else:
-        country = "Worldwide"
-        active = covid.get_total_active_cases()
-        confirmed = covid.get_total_confirmed_cases()
-        recovered = covid.get_total_recovered()
-        deaths = covid.get_total_deaths()
-        string = covid_str.format(country=country,
-                                  active=active,
-                                  confirmed=confirmed,
-                                  recovered=recovered,
-                                  deaths=deaths,
-                                  critical='?')
-        await event.answer(string)
+        output_text = "`No information yet about this country!`"
+    await message.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+    # TODO : send location of country
+    # await client.send_location(message.chat.id, float(country_data["latitude"]), float(country_data["longitude"]))
+
+
+def get_country_data(country, world):
+    for country_data in world:
+        if country_data["country"] == country:
+            return country_data
+    return
     
     
 CMD_HELP.update({
