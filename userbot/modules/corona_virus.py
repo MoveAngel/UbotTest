6788ddc4b5b688af9a12933506ce2cbb7dac47cc
MoveAngel <moveangel29@gmail.com>
2020-04-01 00:58:43 +0700

    Test

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -11,25 +11,27 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.covid(?: |$)(.*)")
+@register(outgoing=True, pattern="^.covid (.*)")
 async def corona(event):
     await event.edit("Processing...")
-    country = event.pattern_match.group(1).split()
+    country = event.pattern_match.group(1)
     covid = Covid()
-    country_cases = covid.get_status_by_country_name(country)
-    if country_cases:
-        output_text = "Confirmed   : {}\n".format(country_cases["confirmed"])
-        output_text += "`Active      : {}`\n".format(country_cases["active"])
-        output_text += "`Deaths      : {}`\n".format(country_cases["deaths"])
-        output_text += "`Recovered   : {}`\n".format(country_cases["recovered"])
-        output_text += "`Last update : {}`\n". \
-            format(datetime.utcfromtimestamp(country_cases["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+    country_data = covid.get_status_by_country_name(country)
+    if country_data:
+        output_text = f"Confirmed   : {country_data['confirmed']}\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text += (
+            "`Last update : "
+            f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
+        )
         output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
     else:
         output_text = "No information yet about this country!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
-    
-    
+
+
 CMD_HELP.update({
         "covid": 
         ".covid <country>"
