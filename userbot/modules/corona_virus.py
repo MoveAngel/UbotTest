8935b3ca70790629ceecb1fdfd802f14e767601e
MoveAngel <moveangel29@gmail.com>
2020-03-31 23:39:17 +0700

    Test

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -5,29 +5,21 @@
 #
 # Port to UserBot by @MoveAngel
 
-import os
-import shutil
 from datetime import datetime
-
-import requests
 from covid import Covid
-
-from userbot import bot, CMD_HELP
+from userbot import CMD_HELP
 from userbot.events import register
 
 
 @register(outgoing=True, pattern="^.covid(?: |$)(.*)")
 async def corona(event):
-    await event.edit("`Processing...`")
-    args = event.text.split(None, 1)
-    if len(args) == 1:
-        return
+    await event.edit("Processing...")
+    country = event.pattern_match.group(1).split()
     covid = Covid()
     data = covid.get_data()
-    country = args[1]
     country_data = get_country_data(country.capitalize(), data)
     if country_data:
-        output_text = "`Confirmed   : {}\n`".format(country_data["confirmed"])
+        output_text = "Confirmed   : {}\n".format(country_data["confirmed"])
         output_text += "`Active      : {}`\n".format(country_data["active"])
         output_text += "`Deaths      : {}`\n".format(country_data["deaths"])
         output_text += "`Recovered   : {}`\n".format(country_data["recovered"])
@@ -35,8 +27,8 @@ async def corona(event):
             format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
         output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
     else:
-        output_text = "`No information yet about this country!`"
-    await event.edit("**Corona Virus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+        output_text = "No information yet about this country!"
+    await event.edit("Corona Virus Info in {}:\n\n{}".format(country.capitalize(), output_text))
 
 def get_country_data(country, world):
     for country_data in world:
