028e2aaf3174ab2ab3fcb1cb781b91937b3a92ed
MoveAngel <moveangel29@gmail.com>
2020-04-01 00:08:52 +0700

    Test

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -16,29 +16,22 @@ async def corona(event):
     await event.edit("Processing...")
     country = event.pattern_match.group(1).split()
     covid = Covid()
-    data = covid.get_data()
-    country_data = get_country_data(country.capitalize(), data)
-    if country_data:
-        output_text = "Confirmed   : {}\n".format(country_data["confirmed"])
-        output_text += "`Active      : {}`\n".format(country_data["active"])
-        output_text += "`Deaths      : {}`\n".format(country_data["deaths"])
-        output_text += "`Recovered   : {}`\n".format(country_data["recovered"])
+    country_cases = covid.get_status_by_country_name(country)
+    if country_cases:
+        output_text = "Confirmed   : {}\n".format(country_cases["confirmed"])
+        output_text += "`Active      : {}`\n".format(country_cases["active"])
+        output_text += "`Deaths      : {}`\n".format(country_cases["deaths"])
+        output_text += "`Recovered   : {}`\n".format(country_cases["recovered"])
         output_text += "`Last update : {}`\n". \
-            format(datetime.utcfromtimestamp(country_data["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+            format(datetime.utcfromtimestamp(country_cases["last_update"] // 1000).strftime('%Y-%m-%d %H:%M:%S'))
         output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
     else:
         output_text = "No information yet about this country!"
     await event.edit("Corona Virus Info in {}:\n\n{}".format(country.capitalize(), output_text))
-
-def get_country_data(country, world):
-    for country_data in world:
-        if country_data["country"] == country:
-            return country_data
-    return
     
     
 CMD_HELP.update({
         "covid": 
-        ".covid <country>\
-          \nUsage: Get an information about data covid-19 in your country.\n"
+        ".covid <country>"
+        "\nUsage: Get an information about data covid-19 in your country.\n"
     })
