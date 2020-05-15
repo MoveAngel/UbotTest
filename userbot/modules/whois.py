0ec8cab2680948444dc16db9cede81ffdc0bf7b2
MoveAngel <moveangel29@gmail.com>
2020-05-15 21:26:13 +0700

    test teros

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -94,20 +94,7 @@ async def get_user(event):
 
 
 async def SeenOnlineStatus(replied_user, user):
-    if replied_user.user.is_bot:
-        return ""
-    elif replied_user.user.status.recently:
-        return "Recently"
-    elif replied_user.user.status.last_week:
-        return "Within the last week"
-    elif replied_user.user.status.last_month:
-        return "Within the last month"
-    elif replied_user.user.status.empty:
-        return "A long time ago"
-    elif replied_user.user.status.online:
-        return "Currently Online"
-    elif replied_user.user.status.offline:
-        return datetime.fromtimestamp(user.status.date).strftime("%a, %d %b %Y, %H:%M:%S")
+    
 
 
 async def fetch_info(replied_user, event):
@@ -136,6 +123,7 @@ async def fetch_info(replied_user, event):
     is_bot = replied_user.user.bot
     restricted = replied_user.user.restricted
     verified = replied_user.user.verified
+    user_status = replied_user.user.status
     photo = await event.client.download_profile_photo(user_id,
                                                       TEMP_DOWNLOAD_DIRECTORY +
                                                       str(user_id) + ".jpg",
@@ -147,6 +135,21 @@ async def fetch_info(replied_user, event):
     username = "@{}".format(username) if username else (
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
+    user_status = user_status.replace(
+                  if user.is_bot:
+                    return ""
+                  elif user.status.recently:
+                    return "Recently"
+                  elif user.status.last_week:
+                    return "Within the last week"
+                  elif user.status.last_month:
+                    return "Within the last month"
+                  elif user.status.empty:
+                    return "A long time ago"
+                  elif user.status.online:
+                    return "Currently Online"
+                  elif user.status.offline:
+                    return datetime.fromtimestamp(user.status.date).strftime("%a, %d %b %Y, %H:%M:%S"))
 
     caption = "<b>USER INFO:</b>\n\n"
     caption += f"First Name: {first_name}\n"
@@ -160,7 +163,7 @@ async def fetch_info(replied_user, event):
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
     caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Last Seen: <code>{SeenOnlineStatus}</code>\n"
+    caption += f"Last Seen: <code>{user_status}</code>\n"
     caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
